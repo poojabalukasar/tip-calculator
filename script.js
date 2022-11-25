b72a7ff79a7249f1a20be23b565f5c01797ac9f8
poojakasar <96623437+poojabalukasar@users.noreply.github.com>
2022-11-25 21:03:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ const calculateTip = () => {
     
     // If user not provide correct input
 
-     if(amount === '' ||amount == 0 || amount < 0){
+     if(amount === '' ||amount === 0 || amount < 0){
         document.getElementById("billEmpty").innerHTML = "Please Enter the Valid Bill Amount";
         return;
      }
      
 
-    if(tipPercentage === ''|| tipPercentage < 0){
+    if(tipPercentage === ''|| tipPercentage === 0 || tipPercentage < 0){
     document.getElementById("tipEmpty").innerHTML ="Please Enter Tip";
      return;
     }
@@ -21,7 +21,7 @@ const calculateTip = () => {
 
     let numPeople = document.getElementById("numPeople").value;
 
-     if(numPeople === 0 || numPeople ==='' || numPeople < 0){
+     if(numPeople === 0 || numPeople === '' || numPeople < 0){
       document.getElementById("zeroPeople").innerHTML = "Enter the Valid Number";
         return;
     }
